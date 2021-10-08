2d61c0f46f03e8b617c4282b8b57401e70473c06
asuravadha <rr.easwar@gmail.com>
2021-10-08 13:09:11 +0530

    threading concepts

--- src/oct08/thread_pract.java
+++ src/oct08/thread_pract.java
@@ -1,17 +1,18 @@
 package oct08;
 
-public class thread_pract extends Thread {
+//through a runnable interface
+public class thread_pract implements Runnable {
     static int threadcount=0;
     public void run(){
-       System.out.println("starting a thread.."+threadcount);
+        System.out.println("starting a thread.."+threadcount);
     }
     public static void main(String[] args){
-        thread_pract thread1=new thread_pract();
-        thread_pract thread2=new thread_pract();
+        thread_pract objt=new thread_pract();
+        Thread thread1=new Thread(objt);
+        Thread thread2=new Thread(objt);
         try{
             thread1.start();
             while(thread1.isAlive()){
-
             }
             threadcount++;
             thread2.start();
@@ -21,3 +22,28 @@ public class thread_pract extends Thread {
 
     }
 }
+
+
+
+//by extending thread class
+//public class thread_pract extends Thread {
+//    static int threadcount=0;
+//    public void run(){
+//       System.out.println("starting a thread.."+threadcount);
+//    }
+//    public static void main(String[] args){
+//        thread_pract thread1=new thread_pract();
+//        thread_pract thread2=new thread_pract();
+//        try{
+//            thread1.start();
+//            while(thread1.isAlive()){
+//
+//            }
+//            threadcount++;
+//            thread2.start();
+//        }catch(Exception e){
+//            System.out.println(e);
+//        }
+//
+//    }
+//}
