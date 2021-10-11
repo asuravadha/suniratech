397d2532017d268b63bf184e91e46f0837a7dc92
asuravadha <rr.easwar@gmail.com>
2021-10-11 11:03:04 +0530

    file handling

---
+++ src/oct11/Main.java
@@ -0,0 +1,47 @@
+package oct11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args)  {
+//file handling
+//        //creating a file
+//        File file=new File("files.txt");
+//        try {
+//            if(file.createNewFile()){
+//                System.out.println("file created ");
+//            }else{
+//                System.out.println("file already exist");
+//            }
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+
+
+////reading data
+//        HashMap<String,String> usercred=new HashMap<>();
+//        File file=new File("files.txt");
+//        try {
+//            Scanner input=new Scanner(file);
+//            while(input.hasNextLine()){
+//                String data=input.nextLine();
+//                String[] up=data.split(" ",2);
+//                usercred.put(up[0],up[1]);
+//            }
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+
+
+    }
+
+
+
+}
